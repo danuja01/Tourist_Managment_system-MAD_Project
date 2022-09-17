a2250a2c31e400006df432e6f7d7ab79dec21a5b
danuja01 <it21023378@my.sliit.lk>
2022-09-17 23:28:10 +0530

    Created UI of the MEDICAL ID Page

--- app/src/main/java/com/example/madproject/MainActivity.java
+++ app/src/main/java/com/example/madproject/MainActivity.java
@@ -5,25 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.madproject.Medical.HealthTracker;
+import com.example.madproject.Medical.MedId;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btn);
     }
 
 
     public void click(View v){
         Intent intent = new Intent(this, HealthTracker.class);
+        startActivity(intent);
+    }
+
+    public void clickBtn(View v){
+        Intent intent = new Intent(this, MedId.class);
         startActivity(intent);
     }
 }
