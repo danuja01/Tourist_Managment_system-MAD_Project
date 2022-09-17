4dfa97dd0fa429b9535129b7da3f89fe7a882f2f
danuja01 <it21023378@my.sliit.lk>
2022-09-17 15:18:27 +0530

    Created UI of the HEALTH TRACKER main page

--- app/src/main/java/com/example/madproject/MainActivity.java
+++ app/src/main/java/com/example/madproject/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.madproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.madproject.Medical.EmptyMedId;
+import com.example.madproject.Medical.HealthTracker;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+    }
+
+
+    public void click(View v){
+        Intent intent = new Intent(this, HealthTracker.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/madproject/Medical/HealthTracker.java
+++ app/src/main/java/com/example/madproject/Medical/HealthTracker.java
@@ -1,8 +1,10 @@
-package com.example.madproject;
+package com.example.madproject.Medical;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.madproject.R;
 
 public class HealthTracker extends AppCompatActivity {
 
