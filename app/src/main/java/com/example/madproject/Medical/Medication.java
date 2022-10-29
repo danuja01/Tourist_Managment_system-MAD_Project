7037a94919f920085eb6ca6d51745f48ce812b3a
danuja01 <it21023378@my.sliit.lk>
2022-10-29 16:11:19 +0530

    Added Medication insert operations

--- app/src/main/java/com/example/madproject/Medical/Medication.java
+++ app/src/main/java/com/example/madproject/Medical/Medication.java
@@ -1,52 +1,155 @@
 package com.example.madproject.Medical;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.madproject.R;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 
 public class Medication extends AppCompatActivity {
 
-     RecyclerView recyclerView;
-    MedicationAdapter medicationAdapter;
+    private RecyclerView recyclerView;
+    private FloatingActionButton fab;
+
+    private DatabaseReference reference;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private String onlineUserId;
+
+    private ProgressDialog loader;
+
+    private String key = "";
+    private String medicationName;
+    private String time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_medication);
+
+        mAuth = FirebaseAuth.getInstance();
+
         recyclerView = findViewById(R.id.medTools);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        FirebaseRecyclerOptions<MedicationModel> options =
-                new FirebaseRecyclerOptions.Builder<MedicationModel>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("medication"), MedicationModel.class)
-                        .build();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
 
-        medicationAdapter = new MedicationAdapter(options);
-        recyclerView.setAdapter(medicationAdapter);
+        loader = new ProgressDialog(this);
+
+        mUser = mAuth.getCurrentUser();
+        onlineUserId = mUser.getUid();
+        reference = FirebaseDatabase.getInstance().getReference().child("medication").child(onlineUserId);
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTask();
+            }
+        });
+
 
 
     }
+
+    public void addTask() {
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        View myView = inflater.inflate(R.layout.input_medication, null);
+        myDialog.setView(myView);
+
+        final AlertDialog dialog = myDialog.create();
+        dialog.setCancelable(false);
+
+        final EditText medication = myView.findViewById(R.id.medcineName);
+        final EditText time = myView.findViewById(R.id.medicationTime);
+        Button save = myView.findViewById(R.id.mediSubmit);
+        Button cancel = myView.findViewById(R.id.mediCancel);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mMedication = medication.getText().toString().trim();
+                String mTime = time.getText().toString().trim();
+                String id = reference.push().getKey();
+
+                if (mMedication.isEmpty()) {
+                    medication.setError("Required Field..");
+                    return;
+                }
+
+                if (mTime.isEmpty()) {
+                    time.setError("Required Field..");
+                    return;
+                }else{
+                    loader.setMessage("Adding your data");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+
+                    MedicationModel model = new MedicationModel(mMedication, "morning", mTime, id);
+                    reference.child(id).setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(Medication.this, "Data Added", Toast.LENGTH_SHORT).show();
+                                loader.dismiss();
+                            } else {
+                                String error = task.getException().toString();
+                                Toast.makeText(Medication.this, "Failed" + error, Toast.LENGTH_SHORT).show();
+                                loader.dismiss();
+                            }
+                        }
+                    });
+                }
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
 
 
     @Override
     protected void onStart() {
         super.onStart();
-        medicationAdapter.startListening();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        medicationAdapter.stopListening();
     }
 
 
